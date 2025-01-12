5cee67a7f872fd864bcc20f6a318e745117be269
seo-onion <sebastianhernandezminano@gmail.com>
2025-01-12 17:25:56 -0500

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
